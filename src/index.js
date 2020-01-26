f351d750b065cabc48cd1a05944ed4fb6bf91c73
Thalles Rodrigues <thalles.cezar@gmail.com>
2020-01-26 20:08:15 -0300

    Start initial Apollo Server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello GraphQL");
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      hello: () => "Hello GraphQL!"
+    }
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log("Listening at " + url);
+});
