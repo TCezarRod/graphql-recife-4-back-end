4b264ce3ba7911a22badc5e15ed093e04b4fd1c0
Thalles Rodrigues <thalles.cezar@gmail.com>
2020-01-27 20:48:33 -0300

    Add fake DB and a few resolvers

---
+++ src/db.js
@@ -0,0 +1,14 @@
+module.exports = {
+  places: [
+    {
+      id: 1,
+      name: "Subway",
+      address: {
+        line1: "Rua da subway",
+        city: "Recife",
+        country: "Brasil"
+      }
+    }
+  ],
+  lastId: 1
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require("apollo-server");
 const { importSchema } = require("graphql-import");
 
+const db = require("./db");
+
 const typeDefs = importSchema("src/schema.graphql");
 
 const server = new ApolloServer({
@@ -8,9 +10,35 @@ const server = new ApolloServer({
   resolvers: {
     Query: {
       places: () => {
-        console.log("Query places");
+        return db.places;
+      }
+    },
+    Mutation: {
+      createPlace: (_root, { place }) => {
+        const newPlace = { ...place, id: ++db.lastId };
+        db.places.push(newPlace);
 
-        return [];
+        return newPlace;
+      }
+    },
+    Place: {
+      name: obj => {
+        return obj.name || db.places.find(place => place.id === obj.id).name;
+      },
+      address: obj => {
+        return (
+          obj.address || db.places.find(place => place.id === obj.id).address
+        );
+      },
+      competitors: obj => {
+        if (obj.competitors) return obj.competitors;
+
+        const competitorIDs =
+          db.places.find(place => place.id === obj.id).competitorIDs || [];
+
+        return competitorIDs.map(id => ({
+          id: Number.parseInt(id)
+        }));
       }
     }
   }
